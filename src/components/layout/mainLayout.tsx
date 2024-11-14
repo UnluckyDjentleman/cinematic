b8b7d35c3c15c38670c34de146b87d939e3e990d
dalvhikki <v.goroshchenja@gmail.com>
2024-11-14 22:51:34 +0300

    one more fix

--- src/components/layout/mainLayout.tsx
+++ src/components/layout/mainLayout.tsx
@@ -1,43 +1,45 @@
-import { AppShell, Image, Burger, Loader } from '@mantine/core'
-import Logo from '../../constants/images/logo.png'
-import { useClickOutside } from '@mantine/hooks'
-import NavbarMenu from '../navbarMenu/navbarMenu'
-import { useState } from 'react'
-import UserPanel from '../userPanel/userPanel'
-import { useAppDispatch } from '../../utils/hooks/reduxStore'
-import useAuth from '../../utils/hooks/useAuth'
-import authState from '../../constants/types/authState'
-import { setUser } from '../../store/reducers/userReducer'
+import { AppShell, Image, Burger } from "@mantine/core";
+import Logo from "../../constants/images/logo.png";
+import { useClickOutside } from "@mantine/hooks";
+import NavbarMenu from "../navbarMenu/navbarMenu";
+import { useState } from "react";
 
-export default function MainLayout({children}:{children: React.ReactNode}){
+export default function MainLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [opened, setOpened] = useState<boolean>(false);
+  const ref = useClickOutside(() => setOpened(false), ["mouseup", "touchend"]);
 
-    const [opened, setOpened] = useState<boolean>(false);
-    const ref=useClickOutside(()=>setOpened(false),['mouseup', 'touchend'])
+  console.log(opened);
 
-    console.log(opened);
-
-    return (
-      <>
+  return (
+    <>
       <AppShell
-      navbar={{
-        width:  { sm: 200, lg: 300 },
-        breakpoint: "sm",
-        collapsed: { mobile: !opened },
-      }}>
+        navbar={{
+          width: { sm: 200, lg: 300 },
+          breakpoint: "sm",
+          collapsed: { mobile: !opened },
+        }}
+      >
         <Burger
-            ref={ref}
-            onClick={()=>setOpened(true)}
-            hiddenFrom="sm"
-            size="sm"
-            style={{position: "sticky"}} />
-          <AppShell.Navbar bg="violet.1" withBorder={false} style={{width:200}}>
-            <Image src={Logo}></Image>
-            <NavbarMenu />
-          </AppShell.Navbar>
-          <AppShell.Main>
-                  {children}
-          </AppShell.Main>
+          ref={ref}
+          onClick={() => setOpened(true)}
+          hiddenFrom="sm"
+          size="sm"
+          style={{ position: "sticky" }}
+        />
+        <AppShell.Navbar
+          bg="violet.1"
+          withBorder={false}
+          style={{ width: 200 }}
+        >
+          <Image src={Logo}></Image>
+          <NavbarMenu />
+        </AppShell.Navbar>
+        <AppShell.Main>{children}</AppShell.Main>
       </AppShell>
-      </>
-    )
+    </>
+  );
 }
